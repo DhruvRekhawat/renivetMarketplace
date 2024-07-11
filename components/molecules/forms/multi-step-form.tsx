82eb6ffb7e42c946f0aa0c88c6abf21c8b566ed9
Dhruv Rekhawat <110072380+DhruvRekhawat@users.noreply.github.com>
2024-07-11 08:01:38 +0530

    mend

--- components/molecules/forms/multi-step-form.tsx
+++ components/molecules/forms/multi-step-form.tsx
@@ -328,9 +328,9 @@ export default function Form() {
                     autoComplete='websiteUrl'
                     className='block w-full rounded-md border-0 py-1.5 px-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6'
                   />
-                  {errors.email?.message && (
+                  {errors.websiteUrl?.message && (
                     <p className='mt-2 text-sm text-red-400'>
-                      {errors.email.message}
+                      {errors.websiteUrl.message}
                     </p>
                   )}
                 </div>
