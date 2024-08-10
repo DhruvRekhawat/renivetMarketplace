e5d67e88b97b1a58a63da1036d3d960411093b3e
Dhruv Rekhawat <110072380+DhruvRekhawat@users.noreply.github.com>
2024-08-10 17:30:42 +0530

    metadata

--- app/survey/layout.tsx
+++ app/survey/layout.tsx
@@ -4,3 +4,17 @@ export const metadata: Metadata = {
   
     description: 'Help us understand the Sustainable Fashion ecosystem better. Your input is invaluable—please share your thoughts in our brief survey!',
   }
+
+
+  export default function SurveyLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+     <main>
+        {children}
+     </main>
+    );
+  }
+  
