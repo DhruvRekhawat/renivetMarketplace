6db864b0890d96140ae28f09b79e9b89822da5cf
Dhruv Rekhawat <110072380+DhruvRekhawat@users.noreply.github.com>
2024-08-10 17:28:44 +0530

    metadata

---
+++ app/survey/layout.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  
+    description: 'Help us understand the Sustainable Fashion ecosystem better. Your input is invaluable—please share your thoughts in our brief survey!',
+  }
--- app/survey/page.tsx
+++ app/survey/page.tsx
@@ -16,10 +16,7 @@ import { useRouter} from 'next/navigation'
 import { Metadata } from 'next'
 
 
-export const metadata: Metadata = {
-  
-  description: 'Help us understand the Sustainable Fashion ecosystem better. Your input is invaluable—please share your thoughts in our brief survey!',
-}
+
 
 const SurveySchema = z.object({
   // Demographics
